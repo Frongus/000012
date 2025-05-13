2634eb18023612c46a63cfea96fc5b272284a794
„frongus„ <pommes.exe.georg@gmail.com>
2025-05-13 21:08:23 +0000

    second commit

--- web/server/index.js
+++ web/server/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const server = express();
 require('dotenv').config;
+const port = process.env.PORT
+console.log(`Current Server Port: ` + process.env.PORT);
 
-console.log(process.env.PORT);
+server.get('/', require('./routes/pages.js'));
+server.use(express.static('./public'));
 
-server.listen()
+server.listen(port, function(err) {
+    if(err) {
+        return err
+    } else {
+        console.log(`Webinstance running on port ${port}`);
+    }
+});
