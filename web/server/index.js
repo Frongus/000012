83b101d8ccf8c67c7e550f8c3b3df427b5d3675f
„frongus„ <pommes.exe.georg@gmail.com>
2025-05-16 16:30:29 +0000

    #3 commit

--- web/server/index.js
+++ web/server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const server = express();
-require('dotenv').config;
+require('dotenv').config('.env');
 const port = process.env.PORT
 console.log(`Current Server Port: ` + process.env.PORT);
 
 server.get('/', require('./routes/pages.js'));
 server.use(express.static('./public'));
+server.set('view engine', 'hbs');
 
 server.listen(port, function(err) {
     if(err) {
--- web/server/routes/pages.js
+++ web/server/routes/pages.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.send(`200 ok`);
+    res.render('index.hbs');
 });
 
 module.exports = router
